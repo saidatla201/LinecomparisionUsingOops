fca15300789dd92707188b6490d9351e38d9f7f0
saidatla201 <saidatla201@gmail.com>
2023-02-22 12:23:29 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LineComparisionUsingOOps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the line comparision");
+
+            LineComparision obj = new LineComparision();
+
+            obj.Firstlines();
+        }
+    }
+}
